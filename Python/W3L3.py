3caeb587b29afe549e996f597174f890dae61bc6
HRCN18 <mayankss1832@gmail.com>
2025-02-26 17:18:51 +0530

    Merge remote-tracking branch 'origin/main'

--- Python/W3L3.py
+++ Python/W3L3.py
@@ -1,1 +1,31 @@
+#  Linear Search
+# my_list = [1,2,3,45,5,1,216,51,32]
+# num = int(input('Enter Number:'))
+# found = False
+#
+# for num in range(0,len(my_list)):
+# 	if my_list[i] == num:
+# 		found = True
+#
+# if found == True:
+# 	print('Number Found')
+# else:
+# 	print('Number not found')
+
+
+
+
+
+# my_list = [-54,5,8,2,1,3,-12,0,125]
+# min_element = my_list[0]
+# # print(min_element)
+#
+# for i in range(1,len(my_list)):
+# 	if min_element > my_list[i]:
+# 		min_element = my_list[i]
+#
+# print(min_element)
+
+
+
 
